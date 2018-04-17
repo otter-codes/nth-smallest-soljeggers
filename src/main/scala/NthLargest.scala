6913c5b958217f139c5fb6248dfa108e3e8e463b
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-04-17 15:00:03 +0100

    - Added Final Test
    - Added Unit test to test unsorted list
    - changed code to make test pass

--- src/main/scala/NthLargest.scala
+++ src/main/scala/NthLargest.scala
@@ -1,7 +1,7 @@
 object NthLargest extends App {
 
   def findNum(numberList: List[Int], n: Int) : Int = {
-    val removeDups = numberList.distinct.reverse
+    val removeDups = numberList.sorted.distinct.reverse
 
     if (n <= 0 || n > removeDups.length)
       throw new IndexOutOfBoundsException("n is larger than the list length")
--- src/test/scala/NthLargestSpec.scala
+++ src/test/scala/NthLargestSpec.scala
@@ -4,13 +4,19 @@ class NthLargestSpec extends WordSpec with MustMatchers {
 
   "NthLargest" must {
 
+    //simplest test
+
     "when given a list of 1 and n equal to 1 return 1" in {
       NthLargest.findNum(List(1), 1) mustEqual 1
     }
 
+    //testing smaller ammount
+
     "when given a list of 1,2,3 and n equal to 2 return 2" in {
       NthLargest.findNum(List(1,2,3) ,2) mustEqual 2
     }
+
+    //set user test
 
     "when given a list of 1,2,3,4,5 and n equal to 2 return 4" in {
       NthLargest.findNum(List(1,2,3,4,5),2) mustEqual 4
@@ -20,10 +26,21 @@ class NthLargestSpec extends WordSpec with MustMatchers {
       NthLargest.findNum(List(3,3,4,4,5,5),3) mustEqual 3
     }
 
+    //testing larger amount
+
     "when given a list of 3,3,4,4,5,5,6,6,7,7 and n equal to 3 return 5" in {
       NthLargest.findNum(List(3,3,4,4,5,5,6,6,7,7), 3) mustEqual 5
 
     }
+
+    //testing if the list is not ordered
+
+    "when given a list of 3,2,5,4,1,7 and n is equal to 2 return 5" in {
+      NthLargest.findNum(List(3,2,5,4,1,7), 2) mustEqual 5
+    }
+
+    //testing exception handling
+
     "when given the list of 1,2 and n equal to 3 throw Exception" in {
      val x =  intercept[IndexOutOfBoundsException] {
         NthLargest.findNum(List(1,2) ,3)
@@ -36,7 +53,6 @@ class NthLargestSpec extends WordSpec with MustMatchers {
       }
       x.getMessage mustEqual "n is larger than the list length"
     }
-
   }
 
 }
